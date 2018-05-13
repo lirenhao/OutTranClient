c0eb0025911f4c0f3dec41677def0b57e5082cc7
lirenhao <renhao.li@bjyada.com>
2018-05-13 15:15:53 +0800

    扫描二维码

--- config/index.js
+++ config/index.js
@@ -55,7 +55,6 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
 
     /**
      * Source Maps
--- src/components/Pay.vue
+++ src/components/Pay.vue
@@ -21,23 +21,61 @@
     },
     methods: {
       click: function (type) {
-        // TODO 调设备扫二维码
-        // this.$router.push({path: `amount/${type}`})
-        this.$router.push({
-          name: 'amount',
-          params: {
-            type,
-            param: {
-              unionMerNo: '',
-              wechatMerNo: '',
-              wechatTermNo: '',
-              sgqrId: '',
-              sgqrPostalCode: '',
-              sgqrMiscellaneous: '',
-              sgqrUnitNumber: ''
+        this.$store.commit('UPDATE_LOADING', true)
+        // 调设备扫二维码
+        cordova.plugins.barcodeScanner.scan(
+          (result) => {
+            if (!result.cancelled) {
+              const tags = this.parse(result.text)
+              const wcTags = this.parse(tags['34'].value)
+              const sgTags = this.parse(tags['51'].value)
+              this.$router.push({
+                name: 'amount',
+                params: {
+                  type,
+                  param: {
+                    unionMerNo: tags['15'].value.substr(16, 15),
+                    wechatMerNo: wcTags['01'].value,
+                    wechatTermNo: wcTags['02'].value,
+                    sgqrId: sgTags['01'].value,
+                    sgqrPostalCode: sgTags['03'].value,
+                    sgqrUnitNumber: sgTags['05'].value,
+                    sgqrMiscellaneous: sgTags['06'].value
+                  }
+                }
+              })
+            } else {
+              this.$store.commit('UPDATE_LOADING', false)
+              this.$vux.toast.show({
+                type: 'warn',
+                position: 'default',
+                text: '扫描已取消'
+              })
             }
+          },
+          (error) => {
+            this.$store.commit('UPDATE_LOADING', false)
+            this.$vux.toast.show({
+              type: 'warn',
+              position: 'default',
+              text: error
+            })
           }
-        })
+        );
+      },
+      parse: function (data) {
+        let start = 0
+        const tags = {}
+        while (start < data.length) {
+          const tag = data.substr(start, 2)
+          start = start + 2
+          const length = parseInt(data.substr(start, 2))
+          start = start + 2
+          const value = data.substr(start, length)
+          start = start + length
+          tags[tag] = {tag, length, value}
+        }
+        return tags
       }
     }
   }
